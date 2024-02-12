ea30c5afd31ef4f9ea8ab588fafcdd28d5d5a8b9
mldev <marcio449@hotmail.com>
2024-02-12 20:25:17 -0300

    Inserção de comentários de mais de uma linha.

---
+++ E03.py
@@ -0,0 +1,6 @@
+# Crie um comentário de mais de uma:
+
+""" Apenas explorando a possibilidade de criar um
+comentário composto de mais de uma linha, que
+permite uma explicação mais elaborada sobre o
+código em questão."""
