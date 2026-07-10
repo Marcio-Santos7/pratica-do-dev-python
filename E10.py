2ea776267fbb7714d1efb918a11d71c9620aca6e
mldev <marcio449@hotmail.com>
2026-07-10 09:53:53 -0300

    feat: adição do programa E10.py

---
+++ E10.py
@@ -0,0 +1,10 @@
+"""Mostre o tamanho da lista nomes e o número de elementos da lista nomes: Mostre 
+separadamente apenas o terceiro elemento dessa lista: """
+
+nomes = ['Ana', 'Carlos', 'Daiane', 'Fernando', 'Maria']
+
+tamanho_lista = len(nomes)
+
+terceiro_elemento = nomes[2]
+
+print(f'O tamanho da lista nomes é {tamanho_lista} e o terceiro elemento é {terceiro_elemento}')
