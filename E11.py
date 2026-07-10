5173ec751cd56e4bf20ca00c503f46e864e4c15d
mldev <marcio449@hotmail.com>
2026-07-10 10:00:23 -0300

    feat: adição do programa E11.py

---
+++ E11.py
@@ -0,0 +1,9 @@
+"""Some os valores das variáveis num1 e num2: Sendo num1 = 52 e num2 = 106. Por fim exiba em 
+tela o resultado da soma."""
+
+num1 = 52
+num2 = 106
+
+soma = num1 + num2
+
+print(f'A soma de num1 e num2 é {soma}')
