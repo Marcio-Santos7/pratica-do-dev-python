6151754a4661caa93877d6c72a80229de1825997
mldev <marcio449@hotmail.com>
2026-07-10 10:09:28 -0300

    feat: adição do programa E12.py

---
+++ E12.py
@@ -0,0 +1,9 @@
+"""Some os valores das variáveis num1 e num2, atribuindo o resultado da soma a uma nova
+variável homônima. Exiba em tela o conteúdo dessa variável."""
+
+num1 = 52
+num2 = 106
+
+soma = num1 + num2
+
+print(soma)
