e732be14d0e90ed9b6b327e1a091b7f3f563f2d5
mldev <marcio449@hotmail.com>
2026-07-10 10:20:32 -0300

    feat: adição do programa E14.py

---
+++ E14.py
@@ -0,0 +1,13 @@
+"""
+Realize as operações de multiplicação e de divisão entre os valores das variáveis num1 e
+num2:
+"""
+
+num1 = 106
+num2 = 52
+
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f'A multiplicação de num1 e num2 é: {multiplicacao}')
+print(f'A divisão de num1 e num2 é: {divisao}')
