dfc13faf7e22f3c4abaad5e9b397ea23f882c9d0
MLdev_Dell <markshow@gmail.com>
2026-07-21 15:07:54 -0300

    feat: criação do programa E16.py

---
+++ E16.py
@@ -0,0 +1,10 @@
+"""Escreva um programa que pede que o usuário dê entrada em dois valores, em seguida, exiba em tela o resultado da soma,
+subtração, multiplicação e divisão desses números:"""
+
+num1 = int(input("Digite o primeiro valor: "))
+num2 = int(input("Digite o segundo valor: "))
+
+print(f'A soma de {num1} com {num2} é: {num1 + num2}')
+print(f'A subtração de {num1} com {num2} é: {num1 - num2}')
+print(f'A multiplicação de {num1} com {num2} é: {num1 * num2}')
+print(f'A divisão de {num1} por {num2} é: {num1 / num2}')
