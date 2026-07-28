757e865a467a0f3e3e889eb0996ec573f0b5f183
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-07-28 00:47:23 -0300

    feat: criação do programa E28.py

---
+++ E28.py
@@ -0,0 +1,7 @@
+"""Crie uma lista com 8 elementos de uma lista de compras de supermercado, por meio de um laço
+de repetição for liste individualmente cada um dos itens dessa lista."""
+
+lista_compras = ['feijão', 'arroz', 'carne', 'frango', 'macarrão', 'tomate', 'pimentão', 'cebola']
+
+for i in lista_compras:
+    print(i)
