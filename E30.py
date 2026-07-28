2ca860c190cf31a9a0ec7d1f99d6238b8c8662e7
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-07-28 01:05:38 -0300

    feat: criação do programa E30.py

---
+++ E30.py
@@ -0,0 +1,11 @@
+"""Crie um programa que realiza a contagem de 0 a 20, exibindo apenas números pares: """
+
+início = 0
+fim = 20
+
+for i in range(0, 21):
+    if i % 2 == 0:
+        print(i)
+    else:
+        pass
+
