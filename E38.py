43dd5a5dbc590cd0a1a63ed2d160a2be5339371a
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-13 10:55:46 -0300

    feat: criação do programa E38.py

---
+++ E38.py
@@ -0,0 +1,6 @@
+"""Peça para que o usuário digite um número, diretamente dentro da função print() eleve esse
+número ao quadrado, exibindo o resultado incorporado a uma mensagem: """
+
+num = int(input("Digite um número: "))
+
+print(f'{num} elevado ao quadrado é igual a: {num**2}')
