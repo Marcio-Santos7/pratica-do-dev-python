3e8966b1ecddf437d3e570f946f6934ddb88b5cf
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-13 11:06:48 -0300

    feat: criação do programa E40.py

---
+++ E40.py
@@ -0,0 +1,8 @@
+"""Adicione o elemento 'Paulo' na lista nomes: 
+nomes = ['Ana', 'Carlos', 'Jamile', 'Fernando', 'Maria']"""
+
+nomes = ['Ana', 'Carlos', 'Jamile', 'Fernando', 'Maria']
+
+nomes.append('Paulo')
+
+print(nomes)
