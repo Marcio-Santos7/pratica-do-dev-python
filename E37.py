6c7b60f4ca37034df9a469e8b3cd1043df0fdc53
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-13 10:36:35 -0300

    feat: criação do programa E37.py

---
+++ E37.py
@@ -0,0 +1,8 @@
+"""Declare uma variável que por sua vez recebe um nome digitado pelo usuário, em seguida, exiba
+em tela uma mensagem de boas-vindas que incorpora o nome anteriormente digitado, fazendo uso
+de f'strings."""
+
+nome = input("Digite o seu nome: ")
+
+print(f'Seja bem vindo {nome} a nossa Empresa !!!')
+
