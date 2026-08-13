c18da717be4a3d52e68eda88954f2d82bc64f3e3
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-13 11:03:05 -0300

    feat: criação do programa E39.py

---
+++ E39.py
@@ -0,0 +1,8 @@
+"""Dada a seguinte lista: nomes = ['Ana', 'Carlos', 'Daiane', 'Fernando', 'Maria'], substitua o 
+terceiro elemento da lista por 'Jamile': """
+
+nomes = ['Ana', 'Carlos', 'Daiane', 'Fernando', 'Maria']
+
+nomes[2] = 'Jamile'
+
+print(nomes)
