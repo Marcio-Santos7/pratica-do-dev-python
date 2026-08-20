e00d233bef390a58725494f630eead7d1a88efc0
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-20 20:54:59 -0300

    feat: criação do programa E43.py

---
+++ E43.py
@@ -0,0 +1,8 @@
+"""Mostre o segundo, terceiro e quarto elemento da lista nomes. Separadamente, mostre apenas o 
+último elemento da lista nomes:"""
+
+nomes = ['Ana', 'Eliana', 'Jamile', 'Fernando', 'Maria', 'Paulo']
+
+print(nomes[1:4])
+
+print(nomes[-1])
