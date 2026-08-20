bcc114aaa6b69191550338226b0f1bd0e9f86025
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-19 23:26:45 -0300

    feat: criação do programa E41.py

---
+++ E41.py
@@ -0,0 +1,9 @@
+"""Adicione o elemento 'Eliana' na lista nomes, especificamente na terceira posição da lista:"""
+
+nomes = ['Ana', 'Carlos', 'Jamile', 'Fernando', 'Maria', 'Paulo']
+
+nomes.insert(2, 'Eliana')
+
+print(nomes)
+
+
