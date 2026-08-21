2be866df7999480dfbaffb2510777df8c49dce6b
Marcio Petro <marcio.leandro@petrobras.com.br>
2026-08-20 21:13:09 -0300

    feat: criação do programa E44.py

---
+++ E44.py
@@ -0,0 +1,10 @@
+"""Crie um dicionário via método construtor dict(), atribuindo para o mesmo ao menos 5 conjuntos
+de chaves e valores representando objetos e seus respectivos preços: """
+
+dicionario = dict(Pão = 'R$ 1,99',
+                  Açúcar = 'R$ 4,99',
+                  Café = 'R$ 3,49',
+                  Macarrão = 'R$ 3,99',
+                  Carne = 'R$ 16,99')
+
+print(dicionario)
