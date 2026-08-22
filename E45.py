34c0ba5fd1215a7a66344451d62f0f715d9c80ce
MLdev_Dell <markshow@gmail.com>
2026-08-22 15:12:40 -0300

    feat: criação do programa E45.py

---
+++ E45.py
@@ -0,0 +1,10 @@
+"""A partir da seguinte lista ['C', 'JavaScript', 'Lua', 'Python'] verifique primeiramente e retorne ao usuário se a 
+linguagem de programação Python consta na lista. Retorne uma mensagem amigável ao usuário para estas duas situações: """
+
+linguagens = ['C', 'JavaScript', 'Lua', 'Python']
+
+for i in linguagens:
+    if i == 'Python':
+        print('A linguagen Python está na lista !!!')
+    else:
+        pass
