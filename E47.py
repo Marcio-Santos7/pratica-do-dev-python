2ed2eb3b0aec3ab9b7150eb236c6022db05ef62a
MLdev_Dell <markshow@gmail.com>
2026-08-22 15:52:22 -0300

    feat: criação do programa E47.py

---
+++ E47.py
@@ -0,0 +1,12 @@
+"""Crie um dicionário usando o construtor de dicionários do Python, alimente os valores do mesmo com os dados de duas listas: """
+
+estados = ['Bahia', 'São Paulo', 'Rio de Janeiro', 'Tocantins', 'Rio Grande do Sul']
+
+capitais = ['Salvador', 'São paulo', 'Rio de Janeiro', 'Palmas', 'Porto Alegre']
+
+dicionario =dict(keys = estados, values = capitais)
+
+print(dicionario)
+print(type(dicionario))
+
+
