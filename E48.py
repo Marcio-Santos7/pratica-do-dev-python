46fa927a37cbc0d2abb2df2a4aa1899d19558204
MLdev_Dell <markshow@gmail.com>
2026-08-24 15:44:08 -0300

    feat: criação do programa E48.py

---
+++ E48.py
@@ -0,0 +1,7 @@
+"""Crie uma simples estrutura de dados simulando um cadastro para uma loja. Nesse cadastro deve conter informações como nome,
+idade, sexo, estado civil, nacionalidade, faixa de renda, etc... Exiba em tela tais dados:"""
+
+cadastro = {'Nome':'Paulo', 'Sexo':'Masculino', 'Idade':38, 'Nacionalidade':'Brasileiro', 'Estado Civil': 'Divorciado', 
+            'Escolaridade':['Ensino Superior', 'Doutorado'], 'Ocupação':'Professor', 'Renda':'1999.00 - 2999.00'}
+
+print(cadastro)
