2e20fe9b352bb119dbf0178cc40bccaef9ad49fa
MLdev_Dell <markshow@gmail.com>
2026-08-26 17:15:54 -0300

    feat: criação do programa E56.py

---
+++ E56.py
@@ -0,0 +1,10 @@
+"""Crie uma função que recebe um valor digitado pelo usuário e eleva esse valor ao quadrado: """
+
+def quadrado(num):
+    return num**2
+
+num = int(input("Digite um número: "))
+
+print(quadrado(num))
+
+
